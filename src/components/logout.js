8632bdfcaeac98060effcab73aad74ba7d4c3a60
EGORDYU <egordyuzhev@gmail.com>
2023-05-22 13:37:48 -0600

    kinda logged me in

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,23 +1,31 @@
-import {useEffect, useState} from "react"
-import axios from "axios";
+import { useEffect } from 'react';
+import axios from 'axios';
+
 export const Logout = () => {
-    useEffect(() => {
-       (async () => {
-         try {
-           const {data} = await  
-                 axios.post('http://localhost:8000/logout/',{
-                 refresh_token:localStorage.getItem('refresh_token')
-                 } ,{headers: {'Content-Type': 'application/json'}},  
-                 {withCredentials: true});
-           localStorage.clear();
-           axios.defaults.headers.common['Authorization'] = null;
-           window.location.href = '/login'
-           } catch (e) {
-             console.log('logout not working', e)
-           }
-         })();
-    }, []);
-    return (
-       <div></div>
-     )
-}
+  useEffect(() => {
+    (async () => {
+      try {
+        const refreshToken = localStorage.getItem('refresh_token');
+
+        await axios.post(
+          'http://localhost:8000/logout/',
+          { refresh_token: refreshToken },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            },
+            withCredentials: true,
+          }
+        );
+
+        localStorage.clear();
+        axios.defaults.headers.common['Authorization'] = null;
+      } catch (error) {
+        console.log('Logout not working', error);
+      }
+    })();
+  }, []);
+
+  return null;
+};
--- src/components/partials/navigations.js
+++ src/components/partials/navigations.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+
 
 export function Navigation() {
    const [isAuth, setIsAuth] = useState(false);
@@ -7,18 +9,19 @@ export function Navigation() {
         setIsAuth(true); 
       }
     }, [isAuth]);
-     return ( 
-      <div>
-        <nav style={{backgroundColor: "dark", color: "white"}}>
-          <a href="/">JWT Authentification</a>            
-          <div> 
-          {isAuth ? <a href="/">Home</a> : null}
-          </div>
-          <div>
-          {isAuth ? <a href="/logout">Logout</a> :  
-                    <a href="/login">Login</a>}
-          </div>
-        </nav>
-       </div>
-     );
+    return (
+        <div>
+          <nav style={{ backgroundColor: 'dark', color: 'white' }}>
+            <Link to="/">JWT Authentification</Link>
+            <div>{isAuth ? <Link to="/">Home</Link> : null}</div>
+            <div>
+              {isAuth ? (
+                <Link to="/logout">Logout</Link>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
+            </div>
+          </nav>
+        </div>
+      );
 }
