47decb5fe0d60543ec064f32f19b0e5be302efd3
EGORDYU <egordyuzhev@gmail.com>
2023-06-21 15:28:08 -0600

    testing logout funtuioncalyl

--- src/components/logout.js
+++ src/components/logout.js
@@ -33,7 +33,7 @@ export const Logout = () => {
         if (response.status === 205) {
             localStorage.clear();
             axios.defaults.headers.common['Authorization'] = null;
-            window.location.href = '/login';
+            // window.location.href = '/login';
         } else {
             console.error('Failed to log out on the server side.');
         }
@@ -48,3 +48,6 @@ export const Logout = () => {
 };
 
 export default Logout;
+
+
+
